bfa233dc0c3d55f0d8354921d307bcf58a387f8d
yazbikod <aslan_murathan@hotmail.com>
2022-10-29 18:14:49 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int aramaYap, veriTut;
+int[] elemanlar = { 32, 52, 1, 3, 5, 87, 12, 35, 26, 46, 88, 15 };
+Array.Sort(elemanlar);  // elemanları sıralayarak .search edebiliriz
+
+
+Console.WriteLine("Arama Yapmak İstediğiniz Değeri Girin: ");
+aramaYap = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Dizi Elemanlarını Yazdıralım");
+for (int i = 0; i < elemanlar.Length; i++)
+    Console.Write("[{0}]", elemanlar[i]);
+
+Console.WriteLine("\nVerinin kaçıncı sırada olduğunu bulalım");
+veriTut = Array.BinarySearch(elemanlar, aramaYap);
+
+Console.WriteLine($"Aradğınız '{aramaYap}' verisi dizinin '{veriTut}' indisinde bulunuyor...");
